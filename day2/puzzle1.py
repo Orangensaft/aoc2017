9da33844c327f1a6ef03ec79220d880a207b1a58
Nicolas Meisberger <orangensaft@outlook.com>
2017-12-03 10:10:49 +0100

    Update puzzle1.py
    Added comments

--- day2/puzzle1.py
+++ day2/puzzle1.py
@@ -2,7 +2,7 @@
 f = open("puzzleinput","r")
 lines = [l.replace("\n","").split("\t") for l in f.readlines()]
 total = 0
-for line in lines:
+for line in lines: #Get min and max of each line and add the difference to total
     l_min = int(min(line,key=lambda x: int(x)))
     l_max = int(max(line,key=lambda x: int(x)))
     diff = l_max-l_min
