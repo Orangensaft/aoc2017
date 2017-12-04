635fa969c1f8064e784f3175ad9a321c4316a266
Nicolas Meisberger <orangensaft@outlook.com>
2017-12-04 08:30:56 +0100

    Added day4

---
+++ day4/puzzle2.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+def isValid(passwd):
+    allWords = set()
+    words = passwd.split(" ")
+    for word in words:
+        word = "".join(sorted(word))
+        if word in allWords:
+            return False
+        allWords.add(word)
+    return True
+f = open("input.txt","r")
+lines = [l.replace("\n","") for l in f.readlines()]
+count = 0
+for line in lines:
+    if isValid(line):
+        count+=1
+        
+print(f"Valid passphrases: {count}")
